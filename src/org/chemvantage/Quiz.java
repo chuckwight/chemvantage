64c779541ad4bb17c2d43c2a77548aed2a1cfb64
chuck.wight <chuck.wight@gmail.com>
2011-07-24 21:28:11 +0000

    Rewrote the print and score routines to make ~10x fewer calls to the datastore (performance enhancement)

--- src/org/chemvantage/Quiz.java
+++ src/org/chemvantage/Quiz.java
@@ -19,9 +19,11 @@ package org.chemvantage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.TimeZone;
 
@@ -150,27 +152,37 @@ public class Quiz extends HttpServlet {
 			Random rand = new Random();  // create random number generator to select quiz questions
 			rand.setSeed(qt.id);  // random number generator seeded with QuizTransaction id value
 			int possibleScore = 0;
-			buf.append("<OL>\n");
 			int nQuestions = (nQuestionsPerSubjectArea < questionKeys.size()?nQuestionsPerSubjectArea:questionKeys.size());
-			for (int i = 0; i < nQuestions; i++) {
-				int n = rand.nextInt(questionKeys.size());
-				try {
-					Question selected = ofy.get(questionKeys.remove(n));
-					possibleScore += selected.pointValue;
-					// the parameterized questions are seeded with a value based on the ids for the quizTransaction and the question
-					// in order to make the value reproducible for grading but variable for each quiz and from one question to the next
-					selected.setParameters((int)(qt.id - selected.id));
-					//buf.append("\n<li>" + selected.print() + "<br></li>\n");
-					buf.append("\n<li>" + (user.hasPremiumAccount()?selected.printPremium():selected.print()) + "<br></li>\n");
-				} catch (Exception e) { // most likely reason is that the question no longer exists in the database; try again.
-					nQuestions = (nQuestionsPerSubjectArea < questionKeys.size()+i?nQuestionsPerSubjectArea:questionKeys.size()+i);
-					i--;
-				}
+			
+			// select nQuestions random keys from questionKeys and store them in keys; then fetch the questions themselves
+			List<Key<Question>> keys = new ArrayList<Key<Question>>();
+			for (int i = 0; i < nQuestions; i++) keys.add(questionKeys.remove(rand.nextInt(questionKeys.size())));
+			List<Question> questions = new ArrayList<Question>(ofy.get(keys).values());
+			
+			// top up the questions because sometimes questions are deleted from the datastore but not from assignments
+			while (questions.size() < keys.size() && questionKeys.size() > 0) {
+				Key<Question> k = questionKeys.remove(rand.nextInt(questionKeys.size()));
+				Question q = ofy.find(k);
+				if (q != null) questions.add(q);
 			}
-			qt.possibleScore = possibleScore;
-			ofy.put(qt);
+			
+			// loop through the questions, presenting them one at a time as the heart of the quiz
+			buf.append("<OL>\n");
+			while (!questions.isEmpty()) {
+				Question selected = questions.remove(0);
+				possibleScore += selected.pointValue;
+				// the parameterized questions are seeded with a value based on the ids for the quizTransaction and the question
+				// in order to make the value reproducible for grading but variable for each quiz and from one question to the next
+				selected.setParameters((int)(qt.id - selected.id));
+				//buf.append("\n<li>" + selected.print() + "<br></li>\n");
+				buf.append("\n<li>" + (user.hasPremiumAccount()?selected.printPremium():selected.print()) + "<br></li>\n");
+			}
 			buf.append("</OL>");
 
+			// update and store the QuizTransaction for this quiz
+			qt.possibleScore = possibleScore;
+			ofy.put(qt);
+			
 			buf.append("\n<input type=hidden name='QuizTransactionId' value=" + qt.id + ">");
 			buf.append("\n<input type=hidden name='TopicId' value=" + topic.id + ">");
 			buf.append("<div id='timer1' style='color: red'></div><div id=ctrl1 style='font-size:50%;color:red;'><a href=javascript:toggleTimers()>hide timers</a><p></div>");
@@ -227,13 +239,9 @@ public class Quiz extends HttpServlet {
 	String printScore(User user,HttpServletRequest request) {
 		StringBuffer buf = new StringBuffer();
 		try {
-			long topicId = Long.parseLong(request.getParameter("TopicId"));
-			Topic topic = ofy.get(Topic.class,topicId);
-
 			Date now = new Date();
 			long transactionId = Long.parseLong(request.getParameter("QuizTransactionId"));
 			QuizTransaction qt = ofy.get(QuizTransaction.class,transactionId);
-			
 			if (qt.graded != null) {
 				return "<h2>No Score</h2>"
 				+ "Sorry, this quiz has been scored already and cannot be scored again. Please consult the <a href=Scores>scores page</a>.";
@@ -249,7 +257,7 @@ public class Quiz extends HttpServlet {
 			int studentScore = 0;
 			int wrongAnswers = 0;
 
-			buf.append("<h2>Quiz Results - " + topic.title + " (" + subject.title + ")</h2>\n");
+			buf.append("<h2>Quiz Results - " + qt.topicTitle + " (" + subject.title + ")</h2>\n");
 			buf.append("<b>" + user.getBothNames() + "</b><br>\n");
 			buf.append(df.format(now));
 
@@ -258,19 +266,24 @@ public class Quiz extends HttpServlet {
 			
 			missedQuestions.append("<OL>");
 
-			for (@SuppressWarnings("unchecked")
-					Enumeration<String> e = request.getParameterNames() ; e.hasMoreElements() ;) {
-				Key<Question> questionKey = null;
+			List<Key<Question>> questionKeys = new ArrayList<Key<Question>>();
+			for (Enumeration<?> e = request.getParameterNames();e.hasMoreElements();) {
 				try {
-					questionKey = new Key<Question>(Question.class,Long.parseLong(e.nextElement()));
-					String studentAnswer[] = request.getParameterValues(Long.toString(questionKey.getId()));
+					questionKeys.add(new Key<Question>(Question.class,Long.parseLong((String) e.nextElement())));
+				} catch (Exception e2) {}
+			}
+			Map<Key<Question>,Question> questions = ofy.get(questionKeys);
+			
+			for (Key<Question> k : questionKeys) {
+				try {
+					String studentAnswer[] = request.getParameterValues(Long.toString(k.getId()));
 					if (studentAnswer != null) {
 						for (int i = 1; i < studentAnswer.length; i++) studentAnswer[0] += studentAnswer[i];
 						if (studentAnswer[0].length() > 0) { // an answer was submitted
-							Question q = ofy.get(questionKey);
+							Question q = questions.get(k);
 							q.setParameters((int)(qt.id - q.id));
 							int score = q.isCorrect(studentAnswer[0])?q.pointValue:0;
-							ofy.put(new Response("Quiz",topic.id,q.id,studentAnswer[0],q.getCorrectAnswer(),score,q.pointValue,user.id,now));
+							ofy.put(new Response("Quiz",qt.topicId,q.id,studentAnswer[0],q.getCorrectAnswer(),score,q.pointValue,user.id,now));
 							studentScore += score;
 							if (score == 0) {  
 								// include question in list of incorrectly answered questions
@@ -286,7 +299,7 @@ public class Quiz extends HttpServlet {
 			missedQuestions.append("</OL>\n");
 			qt.score = studentScore;
 			qt.graded = now;
-			Assignment a = ofy.query(Assignment.class).filter("groupId",user.myGroupId).filter("assignmentType","Quiz").filter("topicId",topicId).get();
+			Assignment a = ofy.query(Assignment.class).filter("groupId",user.myGroupId).filter("assignmentType","Quiz").filter("topicId",qt.topicId).get();
 			if (a != null) ofy.put(Score.getInstance(user.id,a).update(studentScore));
 			ofy.put(qt); // quiz transaction is put after updating score because quizScore had to be recalculated
 			buf.append("<h3>Your score on this quiz is " + studentScore 
@@ -351,7 +364,7 @@ public class Quiz extends HttpServlet {
 				}
 			}
 			buf.append("<FORM METHOD=GET Action=Quiz>"
-					+ "<INPUT TYPE=HIDDEN NAME=TopicId VALUE='" + topic.id + "'>"
+					+ "<INPUT TYPE=HIDDEN NAME=TopicId VALUE='" + qt.topicId + "'>"
 					+ "<INPUT TYPE=SUBMIT VALUE='Take this quiz again'></FORM>\n");
 		} catch (Exception e) {
 			buf.append("Sorry, this quiz could not be scored.<br>" + e.getMessage());
