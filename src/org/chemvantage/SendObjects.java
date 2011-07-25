2bc2cf25141a944e188e5ff58c7b71132b1d5054
chuck.wight <chuck.wight@gmail.com>
2011-07-25 17:10:37 +0000

    ChemVantage LLC Copyright Notice on every src file. GPL version 3 or greater

--- src/org/chemvantage/SendObjects.java
+++ src/org/chemvantage/SendObjects.java
@@ -1,19 +1,20 @@
 /*  ChemVantage - A Java web application for online learning
-    Copyright (C) 2011 ChemVantage LLC
+*   Copyright (C) 2011 ChemVantage LLC
+*   
+*    This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.chemvantage;
 
 import java.io.IOException;
@@ -36,7 +37,7 @@ public class SendObjects extends GenericServlet {
 	@Override
 	public void service(ServletRequest request,ServletResponse response) throws ServletException, IOException {
 		// comment out the body of this service method to disable (normal state)
-		//* FROM HERE.....by removing/restoring the first of 2 slashes on this line
+		/* FROM HERE.....by removing/restoring the first of 2 slashes on this line
         response.setContentType("application/x-java-serialized-object");
         int value = 0;
         try {
