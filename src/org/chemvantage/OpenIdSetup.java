013ff7585f4ccc7a986ddcff03f686f6ad2c9672
chuck.wight <chuck.wight@gmail.com>
2011-12-02 22:13:39 +0000

    Restored OpenId authentication integration scheme using OpenId4Java and the Step2 add-ons that make it work with Google AppEngine

--- src/org/chemvantage/OpenIdSetup.java
+++ src/org/chemvantage/OpenIdSetup.java
@@ -1,28 +1,24 @@
-/*  ChemVantage - A Java web application for online learning
-*   Copyright (C) 2011 ChemVantage LLC
-*   
-*    This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*	This servlet utilizes the OpenID4Java libraries to conduct OpenID authentication	*/
-
+/**
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.chemvantage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -32,146 +28,144 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.openid4java.consumer.ConsumerManager;
-import org.openid4java.consumer.VerificationResult;
+import org.apache.commons.lang.StringUtils;
+import org.chemvantage.samples.apps.marketplace.UserInfo;
+import org.chemvantage.samples.apps.marketplace.openid.ConsumerFactory;
+import org.openid4java.OpenIDException;
+import org.openid4java.consumer.InMemoryConsumerAssociationStore;
 import org.openid4java.discovery.DiscoveryInformation;
-import org.openid4java.discovery.Identifier;
 import org.openid4java.message.AuthRequest;
-import org.openid4java.message.AuthSuccess;
 import org.openid4java.message.ParameterList;
-import org.openid4java.message.ax.FetchRequest;
 
+import com.google.step2.AuthRequestHelper;
+import com.google.step2.AuthResponseHelper;
+import com.google.step2.ConsumerHelper;
+import com.google.step2.Step2;
+import com.google.step2.discovery.IdpIdentifier;
+import com.google.step2.openid.ui.UiMessageRequest;
 import com.googlecode.objectify.Objectify;
 
+/**
+ * Servlet for handling Google Apps Marketplace domain setup. Uses the Step2
+ * library from code.google.com and the underlying OpenID4Java library.
+ */
 public class OpenIdSetup extends HttpServlet {
 
-	private static final long serialVersionUID = 137L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected ConsumerHelper consumerHelper;
 	protected String realm;
-	protected String returnToUrl;
-	protected ConsumerManager manager;
+	protected String returnToPath;
 	DAO dao = new DAO();
 	Objectify ofy = dao.ofy();
 
+	/**
+	 * Init the servlet. For demo purposes, we're just using an in-memory
+	 * version of OpenID4Java's ConsumerAssociationStore. Production apps,
+	 * particularly those in a clustered environment, should consider using an
+	 * implementation backed by shared storage (memcache, DB, etc.)
+	 * 
+	 * @param config
+	 * @throws ServletException
+	 */
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		realm = getInitParameter("realm");
-		if (realm.isEmpty()) realm = "http://www.chemvantage.org";
-		manager = new ConsumerManager();
+		returnToPath = getInitParameter("return_to_path", "/setup");
+		realm = getInitParameter("realm", null);
+		ConsumerFactory factory = new ConsumerFactory(
+				new InMemoryConsumerAssociationStore());
+		consumerHelper = factory.getConsumerHelper();
 	}
 
+	/**
+	 * Either initiates a login to a given provider or processes a response from
+	 * an IDP.
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		if (returnToUrl==null || returnToUrl.isEmpty()) {
-			String returnToPath = getInitParameter("return_to_path");
-			if (returnToPath.isEmpty()) returnToPath = "/setup";
-			returnToUrl = "http://" + request.getServerName() + returnToPath;
-		}
-		String domain = request.getParameter("hd");
-		if (domain != null) { // this is an openId authentication request from a user
+		String domain = req.getParameter("hd");
+		if (domain != null) {
+			// User attempting to login with provided domain, build an OpenID
+			// request and redirect
 			try {
-				// store the callback URL to Google Apps Marketplace in the user's session
-				String callback = request.getParameter("callback");
-				if (callback != null && !callback.isEmpty()) request.getSession().setAttribute("callback",callback);
-				else throw new Exception();
+				AuthRequest authRequest = startAuthentication(domain, req);
+				String url = authRequest.getDestinationUrl(true);
 				
-				// perform discovery on the user-supplied domain
-				List<?> discoveries = new ArrayList<DiscoveryInformation>();
-				discoveries = manager.discover(domain);
-				// attempt to associate with the OpenID provider
-				// and retrieve one service endpoint for authentication
-				DiscoveryInformation discovered = manager.associate(discoveries);
-
-				// store the discovery information in the user's session
-				request.getSession().setAttribute("openid-disc", discovered);
-
-				// obtain a AuthRequest message to be sent to the OpenID provider
-				AuthRequest authReq = manager.authenticate(discovered, returnToUrl);
-
-				// Attribute Exchange example: fetching the user attributes (email, firstName, lastName):
-				FetchRequest fetch = FetchRequest.createFetchRequest();
-				fetch.addAttribute("email", "http://axschema.org/contact/email", true);
-				fetch.addAttribute("firstName", "http://axschema.org/namePerson/first", true);
-				fetch.addAttribute("lastName", "http://axschema.org/namePerson/last", true);
-				// attach the extension to the authentication request
-				authReq.addExtension(fetch);
-				response.sendRedirect(authReq.getDestinationUrl(true));
-			} catch (Exception e) {
-				response.setContentType("text/html");
-				response.getWriter().println("Sorry, the OpenId authentication request failed. - admin@chemvantage.org");
+				String callback = req.getParameter("callback");
+				if (callback != null && !callback.isEmpty()) req.getSession().setAttribute("callback",callback);
+				
+				resp.sendRedirect(url);
+			} catch (OpenIDException e) {
+				resp.sendRedirect("?errorString=Error initializing OpenID request: "
+						+ e.getMessage());
 			}
 		} else {
 			// This is a response from the provider, go ahead and validate
-			doPost(request, response);
+			doPost(req, resp);
 		}
 	}
 
+	/**
+	 * Handle the response from the OpenID Provider.
+	 * 
+	 * @param req
+	 *            Current servlet request
+	 * @param resp
+	 *            Current servlet response
+	 * @throws ServletException
+	 *             if unable to process request
+	 * @throws IOException
+	 *             if unable to process request
+	 */
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession();
 		try {
-            // extract the parameters from the authentication response
-            // (which comes in as a HTTP request from the OpenID provider)
-            ParameterList openIdProviderResponse = new ParameterList(request.getParameterMap());
-
-            // retrieve the previously stored discovery information
-            DiscoveryInformation discovered = (DiscoveryInformation) request.getSession().getAttribute("openid-disc");
-
-            // extract the receiving URL from the HTTP request
-            StringBuffer receivingURL = request.getRequestURL();
-            String queryString = request.getQueryString();
-            if (queryString != null && queryString.length() > 0) receivingURL.append("?").append(request.getQueryString());
-
-            // verify the response; ConsumerManager needs to be the same
-            // (static) instance used to place the authentication request
-            VerificationResult verification = manager.verify(receivingURL.toString(), openIdProviderResponse, discovered);
-
-            // examine the verification result and authenticate the user (creating a new one, if necessary)
-            Identifier verified = verification.getVerifiedId();
-            if (verified != null) {
-            	User user = ofy.find(User.class,verified.getIdentifier());
-            	if (user==null) {
-            		AuthSuccess authResponse = (AuthSuccess) verification.getAuthResponse();
-            		user = User.createOpenIdUser(authResponse);
-            	}
-            	session.setAttribute("UserId", user.getId());
-            	
-            	// create a new ChemVantage domain, if necessary
-            	Domain domain = ofy.query(Domain.class).filter("domainName",user.domain).get();
-    			if (domain == null) {
-    				domain = new Domain(user.domain);
-    				domain.addAdmin(user.id);
-    				ofy.put(domain);
-    				user.setIsAdministrator(true);
-    				user.setIsInstructor(true);
-    				ofy.put(user);
-    			}
-    		
-            	// try to set a Cookie with the user's ID provider:
-    			Cookie c = new Cookie("IDProvider","Google");
-    			c.setMaxAge(2592000); // expires after 30 days (in seconds)
-    			response.addCookie(c);
-    			
-    			// retrieve the callback URL from the user's session, if it exists
-    			String callback = (String)session.getAttribute("callback");
-    			if (callback==null) callback = "";
-    			session.removeAttribute("callback");
-    			
-    			if (callback.isEmpty()) {
-    				response.setContentType("text/html");
-    				PrintWriter out = response.getWriter();
-    				out.println(Home.getHeader(user) + welcomeMessage(user,domain) + Home.footer);
-    			} else response.sendRedirect(callback);
-    			
-            } else throw new Exception();
+			HttpSession session = req.getSession();
+			String callback = (String)session.getAttribute("callback");
+			if (callback==null) callback = "";
+			session.removeAttribute("callback");
+			
+			UserInfo userInfo = completeAuthentication(req);
+			User user = ofy.find(User.class, userInfo.getClaimedId());
+			if (user == null) user = User.createOpenIdUser(userInfo);
+			if (!callback.isEmpty()) {  // on initial setup assign the domain administrator privileges
+				user.setIsAdministrator(true);
+				user.setIsInstructor(true);
+			}
+			ofy.put(user);
+			session.setAttribute("UserId", user.getId());
+			
+			Domain domain = ofy.query(Domain.class).filter("domainName",user.domain).get();
+			if (domain == null) {
+				domain = new Domain(user.domain);
+				if (user.isAdministrator()) domain.addAdmin(user.id);
+				ofy.put(domain);
+			}
+		
+			// try to set a Cookie with the user's ID provider:
+			Cookie c = new Cookie("IDProvider", "Google");
+			c.setMaxAge(2592000); // expires after 30 days (in seconds)
+			resp.addCookie(c);
+			
+			if (callback.isEmpty()) {
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+			out.println(Home.getHeader(user) + welcomeMessage(user,domain) + Home.footer);
+			} else resp.sendRedirect(callback);
+			
+		} catch (OpenIDException e) {
+			resp.sendRedirect("http://" + req.getServerName());
 		}
-		catch (Exception e) {
-			session.invalidate();
-            response.setContentType("text/html");
-            response.getWriter().println("Sorry, the OpenID authentication request failed. - admin@chemvantage.org");
-        }
 	}
 
 	String welcomeMessage(User user,Domain domain) {
@@ -182,10 +176,196 @@ public class OpenIdSetup extends HttpServlet {
 		for (String s : domain.domainAdmins) {
 			buf.append("<tr><td style='text-align:right'>Administrator: </td><td>" + User.getBothNames(s) + " (" + User.getEmail(s) + ")</td></tr>");
 		}
-		buf.append("<tr><td style='text-align:right'>Free trial period expires: </td><td>" + domain.freeTrialExpires.toString() + "</td></tr>");
+		Date now = new Date();
+		if (domain.freeTrialExpires.after(now)) {
+			buf.append("<tr><td style='text-align:right'>Free trial period expires: </td><td>" + domain.freeTrialExpires.toString() + "</td></tr>");
+		}
 		buf.append("</table>");
 		
 		buf.append("<p><span style='color:red;font-weight:bold'>Your account setup is complete.</span><p>");
 		return buf.toString();
 	}
+	/**
+	 * Builds an auth request for a given OpenID provider.
+	 * 
+	 * @param op
+	 *            OpenID Provider URL. In the context of Google Apps, this can
+	 *            be a naked domain name such as "saasycompany.com". The length
+	 *            of the domain can exceed 100 chars.
+	 * @param request
+	 *            Current servlet request
+	 * @return Auth request
+	 * @throws org.openid4java.OpenIDException
+	 *             if unable to discover the OpenID endpoint
+	 */
+	AuthRequest startAuthentication(String op, HttpServletRequest request)
+			throws OpenIDException {
+		IdpIdentifier openId = new IdpIdentifier(op);
+
+		String realm = realm(request);
+		String returnToUrl = returnTo(request);
+
+		AuthRequestHelper helper = consumerHelper.getAuthRequestHelper(openId,
+				returnToUrl);
+		addAttributes(helper);
+
+		HttpSession session = request.getSession();
+		AuthRequest authReq = helper.generateRequest();
+		authReq.setRealm(realm);
+
+		UiMessageRequest uiExtension = new UiMessageRequest();
+		uiExtension.setIconRequest(true);
+		authReq.addExtension(uiExtension);
+
+		session.setAttribute("discovered", helper.getDiscoveryInformation());
+		return authReq;
+	}
+
+	/**
+	 * Validates the response to an auth request, returning an authenticated
+	 * user object if successful.
+	 * 
+	 * @param request
+	 *            Current servlet request
+	 * @return User
+	 * @throws org.openid4java.OpenIDException
+	 *             if unable to verify response
+	 */
+
+	UserInfo completeAuthentication(HttpServletRequest request)
+			throws OpenIDException {
+		HttpSession session = request.getSession();
+		ParameterList openidResp = Step2.getParameterList(request);
+		String receivingUrl = currentUrl(request);
+		DiscoveryInformation discovered = (DiscoveryInformation) session
+				.getAttribute("discovered");
+
+		AuthResponseHelper authResponse = consumerHelper.verify(receivingUrl,
+				openidResp, discovered);
+		if (authResponse.getAuthResultType() == AuthResponseHelper.ResultType.AUTH_SUCCESS) {
+			return onSuccess(authResponse, request);
+		}
+		return onFail(authResponse, request);
+	}
+
+	/**
+	 * Adds the requested AX attributes to the request
+	 * 
+	 * @param helper
+	 *            Request builder
+	 */
+	void addAttributes(AuthRequestHelper helper) {
+		helper.requestAxAttribute(Step2.AxSchema.EMAIL, true)
+				.requestAxAttribute(Step2.AxSchema.FIRST_NAME, true)
+				.requestAxAttribute(Step2.AxSchema.LAST_NAME, true);
+	}
+
+	/**
+	 * Reconstructs the current URL of the request, as sent by the user
+	 * 
+	 * @param request
+	 *            Current servlet request
+	 * @return URL as sent by user
+	 */
+	String currentUrl(HttpServletRequest request) {
+		return Step2.getUrlWithQueryString(request);
+	}
+
+	/**
+	 * Gets the realm to advertise to the IDP. If not specified in the servlet
+	 * configuration. it dynamically constructs the realm based on the current
+	 * request.
+	 * 
+	 * @param request
+	 *            Current servlet request
+	 * @return Realm
+	 */
+	String realm(HttpServletRequest request) {
+		if (StringUtils.isNotBlank(realm)) {
+			return realm;
+		} else {
+			return baseUrl(request);
+		}
+	}
+
+	/**
+	 * Gets the <code>openid.return_to</code> URL to advertise to the IDP.
+	 * Dynamically constructs the URL based on the current request.
+	 * 
+	 * @param request
+	 *            Current servlet request
+	 * @return Return to URL
+	 */
+	String returnTo(HttpServletRequest request) {
+		return new StringBuffer(baseUrl(request))
+				.append(request.getContextPath()).append(returnToPath)
+				.toString();
+	}
+
+	/**
+	 * Dynamically constructs the base URL for the application based on the
+	 * current request
+	 * 
+	 * @param request
+	 *            Current servlet request
+	 * @return Base URL (path to servlet context)
+	 */
+	String baseUrl(HttpServletRequest request) {
+		StringBuffer url = new StringBuffer(request.getScheme()).append("://")
+				.append(request.getServerName());
+
+		if ((request.getScheme().equalsIgnoreCase("http") && request
+				.getServerPort() != 80)
+				|| (request.getScheme().equalsIgnoreCase("https") && request
+						.getServerPort() != 443)) {
+			url.append(":").append(request.getServerPort());
+		}
+
+		return url.toString();
+	}
+
+	/**
+	 * Map the OpenID response into a user for our app.
+	 * 
+	 * @param helper
+	 *            Auth response
+	 * @param request
+	 *            Current servlet request
+	 * @return User representation
+	 */
+	UserInfo onSuccess(AuthResponseHelper helper, HttpServletRequest request) {
+		return new UserInfo(helper.getClaimedId().toString(),
+				helper.getAxFetchAttributeValue(Step2.AxSchema.EMAIL),
+				helper.getAxFetchAttributeValue(Step2.AxSchema.FIRST_NAME),
+				helper.getAxFetchAttributeValue(Step2.AxSchema.LAST_NAME));
+	}
+
+	/**
+	 * Handles the case where authentication failed or was canceled. Just a
+	 * no-op here.
+	 * 
+	 * @param helper
+	 *            Auth response
+	 * @param request
+	 *            Current servlet request
+	 * @return User representation
+	 */
+	UserInfo onFail(AuthResponseHelper helper, HttpServletRequest request) {
+		return null;
+	}
+
+	/**
+	 * Small helper for fetching init params with default values
+	 * 
+	 * @param key
+	 *            Parameter to fetch
+	 * @param defaultValue
+	 *            Default value to use if not set in web.xml
+	 * @return Parameter value or defaultValue
+	 */
+	protected String getInitParameter(String key, String defaultValue) {
+		String value = getInitParameter(key);
+		return StringUtils.isBlank(value) ? defaultValue : value;
+	}
+
 }
