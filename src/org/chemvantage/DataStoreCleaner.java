7377daca92462203be037218eea4e903bd51a2d0
chuck.wight <chuck.wight@gmail.com>
2012-12-05 01:34:41 +0000

    Improvements to DataStoreCleaner to prune Users, Sessions and Responses.

--- src/org/chemvantage/DataStoreCleaner.java
+++ src/org/chemvantage/DataStoreCleaner.java
@@ -31,35 +31,59 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
+import com.googlecode.objectify.Objectify;
 //import com.googlecode.objectify.Objectify;
 //import com.googlecode.objectify.Query;
 
 public class DataStoreCleaner extends HttpServlet {
 	private static final long serialVersionUID = 137L;
 	DAO dao = new DAO();
-	//Objectify ofy = dao.ofy();
+	Objectify ofy = dao.ofy();
 	Subject subject = dao.getSubject();
 
 	public String getServletInfo() {
-		return "PZone servlet presents user's detailed scores in the Practice Zone site.";
+		return "ChemVantage servlet that performs daily maintenance of the datastore.";
 	}
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response)
 	throws ServletException, IOException {
 		// This servlet is called by the cron daemon once each day.
-		//cleanUsers();
+		cleanUsers();
 		cleanSessions();
+		cleanResponses();
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("Done.");
 }
-/*	
+	
 	private void cleanUsers() {
-		Query<User> users = ofy.query(User.class);
-		for (User u : users) u.clean();
-		ofy.put(users);
+		final Date now = new Date();
+		final Date sixMonthsAgo = new Date(now.getTime()-15768000000L); 
+		final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService(); 
+		final Query query = new Query("User"); 
+		for (final Entity user : datastore.prepare(query).asIterable(FetchOptions.Builder.withLimit(1000))) {
+			Date lastLogin = (Date) user.getProperty("lastLogin");
+			String aliasId = (String) user.getProperty("alias");
+			if (lastLogin.before(sixMonthsAgo) && aliasExpired(aliasId)) datastore.delete(user.getKey());
+		}
 	}
-*/	
+
+	private boolean aliasExpired(String userId) {
+		if (userId==null || userId.isEmpty()) return true;
+		try {
+			User u = ofy.get(User.class,userId);
+			final Date now = new Date();
+			final Date sixMonthsAgo = new Date(now.getTime()-15768000000L); 
+			if (u.lastLogin.before(sixMonthsAgo) && aliasExpired(u.alias)) {
+				ofy.delete(u);
+				return true;
+			}
+			else return false;
+		} catch (Exception e) {
+			return true;
+		}		
+	}
+
 	private void cleanSessions() {
 		final long now = new Date().getTime(); 
 		final DatastoreService datastore = 
@@ -68,10 +92,25 @@ public class DataStoreCleaner extends HttpServlet {
 		for (final Entity session : 
 			datastore.prepare(query).asIterable(FetchOptions.Builder.withLimit(1000))) 
 		{ 
-			Long expires = (Long) 
-			session.getProperty("_expires"); 
+			Long expires = (Long) session.getProperty("_expires"); 
 			if (expires < now) { 
 				final Key key = session.getKey(); 
+				datastore.delete(key); 
+			}
+		}
+	}
+
+	private void cleanResponses() {
+		final DatastoreService datastore = 
+			DatastoreServiceFactory.getDatastoreService(); 
+		final Query query = new Query("Response"); 
+		for (final Entity response : 
+			datastore.prepare(query).asIterable(FetchOptions.Builder.withLimit(1000))) 
+		{ 
+			String userId = (String) response.getProperty("userId");
+			User user = ofy.find(User.class,userId);
+			if (user==null) { 
+				final Key key = response.getKey(); 
 				datastore.delete(key); 
 			}
 		}
