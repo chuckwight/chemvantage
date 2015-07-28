7b79f86debe12cae83a1fefae37757458f89024c
chuck.wight@gmail.com <chuck.wight@gmail.com@84a10545-6179-44ee-efd7-93e4f2f1f0f2>
2015-07-28 23:03:33 +0000

    Spell checking and Google search capabilities removed from Quiz and Question objects.

--- trunk/src/org/chemvantage/Question.java
+++ trunk/src/org/chemvantage/Question.java
@@ -244,11 +244,9 @@ public class Question implements Serializable {
 		return print("");
 	}
 	
-	String print(String studentAnswer) {  // this method includes functionality to access spell checking and Google searches
+	String print(String studentAnswer) {
 		StringBuffer buf = new StringBuffer();
 		char choice = 'a';
-		//String q = new String(text + " " + tag).replaceAll("\\<[^>]*>","").trim();
-		//String searchURL = "http://www.google.com/search?q=" + q.replace(' ','+');
 		switch (getQuestionType()) {
 		case 1: // Multiple Choice
 			buf.append("<b>" + text + "</b><br>");
@@ -281,13 +279,6 @@ public class Question implements Serializable {
 		case 4: // Fill-in-the-Word
 			buf.append("<b>" + text + "</b><br>");
 			buf.append("<FONT SIZE=-2 COLOR=FF0000>Enter the correct word or phrase:</FONT><br>");
-/*	=========SPELL CHECKING AND GOOGLE SEARCH TEMPORARILY DISABLED===================		
-			buf.append("<div id=status" + this.id + " style='font-size:10px'><button type=button "
-					+ "onClick=\"javascript: document.getElementById('status" + this.id + "')"
-					+ ".innerHTML='checking...';ajaxSpellCheck(" + this.id + ");\">Check Spelling</button>"
-					+ "<a target=_blank href=" + searchURL + ">Google Search</a></div>");
-	==================================================================================
-*/		
 			buf.append("<input id=" + this.id + " type=text name=" + this.id + " value='" + CharHider.quot2html(studentAnswer) + "'>");
 			buf.append("<b>" + tag + "</b><br>");
 			break;
