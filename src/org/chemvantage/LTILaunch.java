63560e637997e586baf2a00d514921c485a5434a
Chuck Wight <admin@chemvantage.org>
2015-08-04 15:28:27 -0600

    Minor changes to BLTI clients

--- src/org/chemvantage/LTILaunch.java
+++ src/org/chemvantage/LTILaunch.java
@@ -162,6 +162,7 @@ public class LTILaunch extends HttpServlet {
 		session.setAttribute("UserId",userId);
 		User user = User.getInstance(session);
 		if (user==null) user = User.createBLTIUser(request); // first-ever login for this user
+		
 		// try to set a cookie in the user's browser with the identity provider
 		Cookie c = new Cookie("IDProvider","BLTI");
 		c.setMaxAge(2592000); // expires after 30 days (in seconds)
