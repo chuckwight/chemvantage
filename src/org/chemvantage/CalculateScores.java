7bcd38b3998899c2162a6ad2eeee74a60bcdf39f
wight <wight@10.0.1.22>
2019-06-01 14:27:31 -0400

    reorganized and cleaned up code for CalculateScores

--- src/org/chemvantage/CalculateScores.java
+++ src/org/chemvantage/CalculateScores.java
@@ -69,16 +69,49 @@ public class CalculateScores extends HttpServlet {
 			return;
 		}
 		String userRequest = request.getParameter("UserRequest");
-		boolean recalculate = (userRequest != null && userRequest.contentEquals("Recalculate"))?true:false;
+		if (userRequest == null) userRequest = ""; // prevents null pointer Exception
 		
-		// view Score objects for one assignment
+		boolean recalculate = (userRequest.contentEquals("Recalculate"))?true:false;
+		
+		if (userRequest.contentEquals("ViewTransactions")) {  // view assignment transactions for a single student
+			User student = ofy().load().type(User.class).id(request.getParameter("UserId")).now();
+			out.println(Home.header + viewTransactions(student,assignment) + Home.footer);
+		} else {  // view group Score objects for one assignment
+			out.println(Home.header + viewGroupScores(assignment,recalculate) + Home.footer); 
+		}
+	}
+	
+	public String viewTransactions(User student,Assignment a) {
+		StringBuffer buf = new StringBuffer();
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.FULL);
 		try {
-			Group group = ofy().load().type(Group.class).id(assignment.groupId).now();
-			if (recalculate) group.reviseScores(assignment);
+			if ("quiz".contentEquals(a.assignmentType)) {
+				List<QuizTransaction> quizTransactions = ofy().load().type(QuizTransaction.class).filter("topicId",a.topicId).filter("userId",student.id).order("downloaded").list();
+				if (quizTransactions.size()==0) return "This user has no transactions for this assignment in the database.";
+				// construct a table of quiz transactions for this user on this topic
+				Topic t = ofy().load().type(Topic.class).id(a.topicId).now();
+				buf.append("<h3>Complete list of quiz transactions on " + t.title + " for user " + student.id + "</h3>");
+				buf.append("<table><th>Downloaded</th><th>Score</th>");
+				for (QuizTransaction qt : quizTransactions) {
+					buf.append("<td>" + df.format(qt.downloaded) + "</td><td align=center>" + (qt.graded==null?"-":qt.score*100./qt.possibleScore + "%") + "</td>");
+				}
+				buf.append("</table>");
+				buf.append("All times are given in Coordinated Universal Time (UTC). If the score is missing, it means that the quiz was downloaded but not submitted for grading within the 15 minute time limit.");
+			}
+		} catch (Exception e) {
+		}
+		return buf.toString();	
+	}
+	
+	public String viewGroupScores(Assignment a,boolean recalculate) {
+		StringBuffer buf = new StringBuffer();
+		try {
+			Group group = ofy().load().type(Group.class).id(a.groupId).now();
+			if (recalculate) group.reviseScores(a);
 
 			DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.FULL);
-			Topic t = ofy().load().type(Topic.class).id(assignment.topicId).now();
-			StringBuffer buf = new StringBuffer("<h3>" + assignment.assignmentType + " - " + t.title + "</h3>");
+			Topic t = ofy().load().type(Topic.class).id(a.topicId).now();
+			buf.append("<h3>" + a.assignmentType + " - " + t.title + "</h3>");
 			buf.append(df.format(new Date()) + "<p>");
 
 			// make a list of student users, removing instructors, admins and TAs
@@ -93,30 +126,29 @@ public class CalculateScores extends HttpServlet {
 			if (groupMembers.size()==0) buf.append("There are no students in this group yet.");
 			else {
 				if (recalculate) {
-						buf.append("These scores have been recalculated just now:<p>");
+					buf.append("These scores have been recalculated just now:<p>");
 				} else buf.append("In most cases ChemVantage does not store names or other personal identifiable information (PII), "
 						+ "so scores are listed only by the user ID supplied by your Learning Management System. If these scores "
-						+ "don't look right, you may <a href=/CalculateScores?UserRequest=Recalculate&AssignmentId=" + assignment.id + ">"
+						+ "don't look right, you may click any student's Name/UserId below to get a complete list of " + a.assignmentType
+						+ " transactions, or <a href=/CalculateScores?UserRequest=Recalculate&AssignmentId=" + a.id + ">"
 						+ "click here to recalculate all student scores for this assignment</a> (this may take a few minutes).<p>");
 				// print a table of scores for this group assignment
 				buf.append("<table cellspacing=0 border=1><tr><th>Name/UserID</th><th>Score</th><th># attempts</th><th>Most Recent Attempt</th></tr>");
 				Queue queue = QueueFactory.getDefaultQueue();
 				for (User u : groupMembers) {
 					counter++;
-					s = u.getScore(assignment);
-					if (s.needsLisReporting()) queue.add(withUrl("/ReportScore").param("AssignmentId",Long.toString(assignment.id)).param("UserId",u.id));
+					s = u.getScore(a);
+					if (s.needsLisReporting()) queue.add(withUrl("/ReportScore").param("AssignmentId",Long.toString(a.id)).param("UserId",u.id));
 					name = u.getFullName();
 					if (name.isEmpty()) name = u.getId().substring(group.domain.length()+1);
 					double pct = s.score*100./s.maxPossibleScore;
-					buf.append("<tr><td>" + name + "</td><td>" + String.format("%.0f", pct) + "%</td><td>" + s.numberOfAttempts + "</td><td>" + (s.mostRecentAttempt==null?"":df.format(s.mostRecentAttempt)) + "</td></tr>");
+					buf.append("<tr><td><a href=/CalculateScores?UserRequest=ViewTransactions&AssignmentId=" + a.groupId + "&UserId=" + u.id + ">" + name + "</a></td><td>" + String.format("%.0f", pct) + "%</td><td>" + s.numberOfAttempts + "</td><td>" + (s.mostRecentAttempt==null?"":df.format(s.mostRecentAttempt)) + "</td></tr>");
 				}
 				buf.append("</table><br>" + counter + " student" + (counter!=1?"s":""));
 			}
 
-			out.println(Home.header + buf.toString() + Home.footer);
 		} catch (Exception e) {
-			out.println(e.getMessage());
 		}
-
+		return buf.toString();
 	}
 }
