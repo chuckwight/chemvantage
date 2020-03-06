2d0d076ac71ea99f2e10540f8f77dcda197f8033
cawight <cawight@131.118.37.144>
2020-03-06 12:33:46 -0500

    Bulletproofing identification of the correct Deployment entity

--- src/org/chemvantage/Token.java
+++ src/org/chemvantage/Token.java
@@ -3,8 +3,8 @@ package org.chemvantage;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -106,29 +106,21 @@ public class Token extends HttpServlet {
 		// However, this is not technically required by the specifications. Hmm.
 		
 		if (deployment_id==null) deployment_id = "";  // may be empty String for 1-deployment platforms
-		if (!platform_id.startsWith("http")) platform_id = "https://" + platform_id; // make it into a URL
-		String platform_deployment_id = platform_id + "/" + deployment_id;
+		
+		URL platform = new URL(platform_id);
+		String platform_deployment_id = new URL("https",platform.getHost(),platform.getPort(),"/"+deployment_id).toString();	
 		
 		// Optimistic route first:
 		Deployment d = ofy().load().type(Deployment.class).id(platform_deployment_id).now();
 		if (d != null) return d;
 	
-		// OK, that didn't work; try a range of Deployments all with the matching platform_id
+		// OK, that didn't work; try a range of Deployments all with the matching platform_id and client_id
 		Key<Deployment> kstart = Key.create(Deployment.class, platform_id);
 		Key<Deployment> kend = Key.create(Deployment.class, platform_id + "~");			
 		
 		if (client_id != null) { // try matching the client_id to any Deployment with matching platform_id
 			d = ofy().load().type(Deployment.class).filterKey(">",kstart).filterKey("<",kend).filter("client_id",client_id).first().now();
 			if (d != null) return d;		
-		} else {  // check to see if there is only one Deployment with this platform_id
-			List<Deployment> d_list = ofy().load().type(Deployment.class).filterKey(">",kstart).filterKey("<",kend).list();
-			if (d_list.size()==0) throw new Exception("This platform is not a registered entity in ChemVantage.");
-			if (d_list.size()==1) return d_list.get(0); // success
-			else { // check to see if multiple deployments all have the same client_id value
-				client_id = d_list.get(0).client_id;
-				for (int i=1;i<d_list.size();i++) if (!d_list.get(i).client_id.contentEquals(client_id)) throw new Exception("Unable to identify Deployment due to multiple security contracts in this platform.");
-				return d_list.get(0);
-			}
 		}
 		
 		// At this point the Deployment does not exist in the datastore
