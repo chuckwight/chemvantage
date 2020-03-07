05a559be801e10343ac4048114de6f07365510c2
wight <wight@localhost>
2020-03-07 12:37:05 -0500

    Streamlined issuance of Auth token for LTI1.3

--- src/org/chemvantage/Token.java
+++ src/org/chemvantage/Token.java
@@ -5,6 +5,7 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,10 +42,7 @@ public class Token extends HttpServlet {
 			if (login_hint == null) throw new Exception("Missing required login_hint parameter.");
 			if (target_link_uri == null) throw new Exception("Missing required target_link_uri parameter.");
 			
-			//if (!platform_id.startsWith("http")) platform_id = "https://" + platform_id;
-			
 			String deployment_id = request.getParameter("lti_deployment_id");
-			if (deployment_id == null) deployment_id = "";
 			debug.append("deployment_id: " + deployment_id + "<br>");
 			
 			String client_id = request.getParameter("client_id");
@@ -110,7 +108,7 @@ public class Token extends HttpServlet {
 		
 		URL platform = new URL(platform_id);
 		String platform_deployment_id = new URL("https",platform.getHost(),platform.getPort(),deployment_id).toString();
-		if (platform_deployment_id.lastIndexOf("/") == platform_deployment_id.length()+1) platform_deployment_id = platform_deployment_id.substring(0, platform_deployment_id.length()-1);
+		if (platform_deployment_id.lastIndexOf("/") == platform_deployment_id.length()-1) platform_deployment_id = platform_deployment_id.substring(0, platform_deployment_id.length()-1);
 		
 		// Optimistic route first:
 		Deployment d = ofy().load().type(Deployment.class).id(platform_deployment_id).now();
@@ -120,13 +118,14 @@ public class Token extends HttpServlet {
 		Key<Deployment> kstart = Key.create(Deployment.class, platform.toString());
 		Key<Deployment> kend = Key.create(Deployment.class, platform.toString() + "~");			
 		
-		if (client_id != null) { // try matching the client_id to any Deployment with matching platform_id
-			d = ofy().load().type(Deployment.class).filterKey(">",kstart).filterKey("<",kend).filter("client_id",client_id).first().now();
-			if (d != null) return d;		
-		}
-		
+		// Check to see if any Deployment matches the platform_id
+		List<Deployment> deployments = ofy().load().type(Deployment.class).filterKey(">=",kstart).filterKey("<",kend).list();
+		if (deployments.size()>0) return deployments.get(0);		
+
 		// At this point the Deployment does not exist in the datastore
-		throw new Exception("ChemVantage was unable to identify " + platform_deployment_id + " as a registered entity, sorry.");
+		throw new Exception("ChemVantage was unable to identify " + platform_deployment_id + " as a registered entity, sorry.<br>"
+				+ "platform: " + platform.toString() + "<br>"
+				+ "start/end: " + kstart.getName() + " / " + kend.getName() + "<br>");
 	}
 	
 
