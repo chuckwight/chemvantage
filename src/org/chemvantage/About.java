bafa011b27c9bf303687b2fe6570b5954dbffa1d
wight <wight@10.0.1.30>
2020-04-03 07:36:15 -0400

    version 2020.04.03 passes all LTI certs v1.1 and LTI Advantage

--- src/org/chemvantage/About.java
+++ src/org/chemvantage/About.java
@@ -1,5 +1,5 @@
 /*  ChemVantage - A Java web application for online learning
-*   Copyright (C) 2011 ChemVantage LLC
+*   Copyright (C) 2020 ChemVantage LLC
 *   
 *    This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
