f44a57c985019773834161f2b3f22684e700995b
Chuck Wight <wight@10.229.115.72>
2020-04-15 11:42:35 -0400

    Finished initial implementation of Help page

--- src/org/chemvantage/Help.java
+++ src/org/chemvantage/Help.java
@@ -21,6 +21,7 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -67,8 +68,26 @@ public class Help extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// This is the entry point for authenticated students to request a tokenized URL to get help with a problem.
+		// Required elements are:
+		// 1) valid id_token (Token)
+		// 2) assignmentType (AssignmentType)
+		// 3) transactionId  (TransactionId)
+		try {
+			User user = User.getUser(request.getParameter("Token"));
+			if (user == null) throw new Exception("Invalid id_token");
+			
+			String jwt = createJWT(request);
+			
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
+			String iss = "https://" + request.getServerName();
+			
+			out.println(Home.header("ChemVantage Help Page") + Home.banner + displayHelpUrl(jwt,iss) + Home.footer);
+			
+		} catch (Exception e) {
+			response.sendError(401, e.getMessage());	
+		}
 	}
 	
 	protected JsonObject validateJWT(String jwt) throws Exception {
@@ -81,7 +100,13 @@ public class Help extends HttpServlet {
 
 	protected String showStudentSubmission(JsonObject payload) throws Exception {
 		StringBuffer buf = new StringBuffer(Home.header("ChemVantage Help Page") + Home.banner);
+		Date exp = new Date(payload.get("exp").getAsLong());
+		
 		buf.append("<h3>A Student is seeking your help with a ChemVantage assignment</h3>");
+		buf.append("The information below may help to illuminate the problem. This information may contain updates since "
+				+ "the student made this request (i.e., they may have solved the problem on their own).<p>"
+				+ "The token contained in the URL you just entered is only valid for 3 days, and will expire at "
+				+ exp + ". Please communicate your response directly to the student, not through ChemVantage.<p>");
 		/*
 		 * The expected contents of the payload are:
 		 *  1) aty (AssignmentType: Q, H, or P)
@@ -104,9 +129,73 @@ public class Help extends HttpServlet {
 				q.setParameters(hashMe.hashCode());  // creates different parameters for different assignments
 				buf.append(q.print(hwt.showWork,""));
 				
+				List<Response> responses = ofy().load().type(Response.class).filter("userId",hwt.userId).filter("questionId",q.id).list();
+				String message = "";
+				if (responses.size() > 0) {
+					buf.append("<table><tr><td>Date/Time (UTC)</td><td>Student Response</td><td>Correct Response</td><td>Score</td></tr>");
+					for (Response r : responses) {
+						if (r.score>0) message = "This question was solved correctly on " + r.submitted;
+						buf.append("<tr><td>" + r.submitted.toString() + "</td><td align=center>" + r.studentResponse 
+							+ "</td><td align=center>" + r.correctAnswer + "</td><td align=center>" + r.score + "</td></tr>");
+						
+					}
+					buf.append("</table>");
+				}
+				buf.append(message);
 				break;
 			default: throw new Exception("The assignment type was not valid.");
 		}
-		return "";
+		buf.append(Home.footer);
+		
+		return buf.toString();
+	}
+	
+	protected String createJWT(HttpServletRequest request) throws Exception {
+		String aty = request.getParameter("AssignmentType");
+		if (aty==null) throw new Exception("Missing assignment type");
+		switch (aty) {
+			case "Quiz": aty = "Q";
+			case "Homework": aty = "H";
+			case "PracticeExam": aty = "P";
+			default: aty = null;
+		}
+		
+		long tid = Long.parseLong(request.getParameter("TransactionId"));
+		Date now = new Date();
+		Date in3Days = new Date(now.getTime() + 259200000L);
+		
+		String token = JWT.create()
+				.withClaim("aty", aty)
+				.withClaim("tid", tid)
+				.withExpiresAt(in3Days)
+				.sign(algorithm);
+		
+		return token;
+	}
+	
+	protected String displayHelpUrl(String jwt, String server) {
+		StringBuffer buf = new StringBuffer();
+		Date exp = JWT.decode(jwt).getExpiresAt();
+		
+		buf.append("<h3>Need Some Help?</h3>");
+		buf.append("Here's what to do: Copy the message below, including the entire URL, and send it with your question via email "
+				+ " to your instructor. This will enable your instructor to view your responses to the question, including "
+				+ "anything that you may have written in the box the was labeled: 'Show your work here'. You can use the link "
+				+ "yourself to verify exactly what information your instructor will see.<p>"
+				+ "Your email should try to explain why you're stuck, if possible. When solving homework problems, it is important "
+				+ "to show your work in the box provided, because that often can illuminate the issue for your instructor.<p>");
+		
+		buf.append("<hr><p>");
+		
+		buf.append("To the instructor:<br>"
+				+ "The student sending this message is having difficulties solving a problem in ChemVantage. Please click the URL"
+				+ "below to view the question item and the student's responses. The link is dynamic will always give current "
+				+ "information, so if the student solved the issue on their own or another way, you will see that in the page.<p>"
+				+ "The token provided in the link is only valid for 3 days and expires at " + exp + ".<p>");
+		
+		buf.append(server + "/help?JWT=" + jwt);
+		
+		buf.append("<p>Thank you.");
+		return buf.toString();
 	}
 }
