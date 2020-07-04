6a540cad2677b4a933193c5af730e67d47588515
wight <wight@localhost>
2020-07-04 14:31:54 -0400

    Cleanup of imports

--- src/org/chemvantage/LTIMessage.java
+++ src/org/chemvantage/LTIMessage.java
@@ -41,7 +41,6 @@ import java.util.UUID;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
