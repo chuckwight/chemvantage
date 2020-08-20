d864b24ddae34fe5a61cb137419e34e15c28490f
wight <wight@localhost>
2020-08-20 16:47:44 -0400

    Simplified Admin page to list only the number of new accounts.

--- src/org/chemvantage/Admin.java
+++ src/org/chemvantage/Admin.java
@@ -22,7 +22,6 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
@@ -126,15 +125,9 @@ public class Admin extends HttpServlet {
 			
 			buf.append("<h3>Recent Activity (past 30 days)</h3>");
 			Date lastMonth = new Date(new Date().getTime()-2592000000L);
-			List<BLTIConsumer> cons = ofy().load().type(BLTIConsumer.class).filter("lastLogin >",lastMonth).list();
-			for (BLTIConsumer c : cons) {
-				buf.append(c.oauth_consumer_key + " (created " + c.created + ")<br>");
-			}
-			List<Deployment> deps = ofy().load().type(Deployment.class).filter("lastLogin >",lastMonth).list();
-			for (Deployment d : deps) {
-				buf.append(d.platform_deployment_id + " (created " + d.created + ")<br>");
-			}
-			buf.append("<br>Total number of Response entities: " + ofy().load().type(Response.class).filter("submitted >",lastMonth).count());
+			buf.append("New Basic LTI Consumer accounts: " + ofy().load().type(BLTIConsumer.class).filter("lastLogin >",lastMonth).count() + "<br>");
+			buf.append("New LTI Advantage deployments: " + ofy().load().type(Deployment.class).filter("lastLogin >",lastMonth).count() + "<br>");
+			buf.append("Total number of Response entities: " + ofy().load().type(Response.class).filter("submitted >",lastMonth).count());
 			
 			buf.append("<h3>Basic LTI Consumer Search</h3>");
 			int nConsumers = ofy().load().type(BLTIConsumer.class).count();
