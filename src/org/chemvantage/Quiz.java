e4a66fafabaef707af21fe43ee53a0685923c022
Chuck Wight <wight@10.229.115.72>
2022-11-19 13:15:13 -0500

    Quiz bug fix for including extra concept in question selection

--- src/org/chemvantage/Quiz.java
+++ src/org/chemvantage/Quiz.java
@@ -893,11 +893,14 @@ public class Quiz extends HttpServlet {
 					break;
 				}
 			} catch (Exception e) {}
-			// next, include a new conceptId if selected:
-			try {
-				conceptIds.add(Long.parseLong(request.getParameter("ConceptId")));
-			} catch (Exception e) {}
 			
+			// next, include any conceptIds included in this request:
+			Long newConcept = null;
+			try {
+				newConcept = Long.parseLong(request.getParameter("ConceptId"));
+				conceptIds.add(newConcept);
+			} catch (Exception e) {}
+
 			// Make a list of key concepts already covered by this Quiz:
 			List<Key<Concept>> conceptKeys = ofy().load().type(Concept.class).keys().list();
 			Map<Key<Concept>,Concept> keyConcepts = ofy().load().keys(conceptKeys);
@@ -909,14 +912,17 @@ public class Quiz extends HttpServlet {
 			// Create a short form to select one additional key concept to include (will exclude the previous selection, if any)
 			buf.append("<form method=get action=/Quiz>"
 					+ "<input type=hidden name=sig value='" + user.getTokenSignature() + "' />"
-					+ "You may include additional question items from another key concept: "
-					+ "<input type=hidden name=UserRequest value=AssignQuizQuestions /><select name=ConceptId>");
+					+ "You may include additional question items from: "
+					+ "<input type=hidden name=UserRequest value=AssignQuizQuestions />"
+					+ "<select name=ConceptId><option value='Select'>Select a key concept</option>");
 			for (Key<Concept> k : conceptKeys) {
-				if (conceptIds.contains(k.getId()) || keyConcepts.get(k).title.startsWith("0")) continue;  // skip current and hidden conceptIds
-				buf.append("<option value=" + k.getId() + ">" + keyConcepts.get(k).title + "</option>");
+				try {
+					if (conceptIds.contains(k.getId()) || keyConcepts.get(k).orderBy.startsWith(" 0")) continue;  // skip current and hidden conceptIds
+					buf.append("<option value='" + k.getId() + "'" + (newConcept!=null && k.getId()==newConcept?" selected>":">") + keyConcepts.get(k).title + "</option>");
+				} catch (Exception e) {}
 			}
-			buf.append("<input type=submit value='Include This Concept' /></form><hr><br/>");
-			
+			buf.append("<input type=submit value='Show questions' /></form><hr><br/>");
+
 			// now we have all of the relevant conceptIds. Make a list of questions carrying these attributes:
 			List<Key<Question>> questionKeys = new ArrayList<Key<Question>>();
 			for (Long cId : conceptIds) questionKeys.addAll(ofy().load().type(Question.class).filter("assignmentType","Quiz").filter("conceptId",cId).keys().list());
