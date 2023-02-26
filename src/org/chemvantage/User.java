f0d848e71297123e77dbb6f3385dc464a76b770d
Charles Wight <chuck.wight@gmail.com>
2023-02-26 18:07:16 -0500

    Bug fix for anonymous users

--- src/org/chemvantage/User.java
+++ src/org/chemvantage/User.java
@@ -98,7 +98,7 @@ public class User {
     	try {  // try to validate an anonymous user
     		Date exp = new Date(encode(Long.parseLong(sig,16)));
     		Date aMonthFromNow = new Date(now.getTime() + 2678400000L);
-    		if (exp.after(now) && exp.before(aMonthFromNow)) return new User(expires.getTime());
+    		if (exp.after(now) && exp.before(aMonthFromNow)) return new User(exp.getTime());
     	} catch (Exception e) {}
 
     	return null;   	
