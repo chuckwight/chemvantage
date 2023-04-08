f2af257053e947b416e8564309b7e24d142cc0b1
Charles Wight <chuck.wight@gmail.com>
2023-04-08 07:05:47 -0400

    Token.getDeployment expanded to work for schoology

--- src/org/chemvantage/Token.java
+++ src/org/chemvantage/Token.java
@@ -39,12 +39,7 @@ public class Token extends HttpServlet {
 			if (login_hint == null) throw new Exception("Missing required login_hint parameter.");
 			if (target_link_uri == null) throw new Exception("Missing required target_link_uri parameter.");
 			
-			String deployment_id = request.getParameter("lti_deployment_id");
-			if (deployment_id==null) deployment_id = request.getParameter("login_hint");
-			String client_id = request.getParameter("client_id");
-			
-			Deployment d = getDeployment(platform_id,deployment_id,client_id);
-			//if (d==null) response.sendRedirect("/Registration.jsp?message=To+start+using+ChemVantage,+please+register+your+LMS.");
+			Deployment d = getDeployment(request);
 			
 			if (d==null) throw new Exception("ChemVantage was unable to identify this deployment from your LMS. "
 					+ "If you received a registration email within the past 7 days, please use the tokenized link in that message to "
@@ -101,76 +96,91 @@ public class Token extends HttpServlet {
 			response.sendError(401,"Failed Auth Token. " + (e.getMessage()==null?e.toString():e.getMessage()) + "\n" + debug.toString());
 		}
 	}
-
-	private Deployment getDeployment(String platform_id,String deployment_id, String client_id) throws Exception {
+	
+	private static Deployment getDeployment(HttpServletRequest request) throws Exception {
 		// This method attempts to identify a unique registered Deployment entity based on the required
 		// platform_id value and the optional lti_deployment_id and client_id values. The latter should 
 		// be used in case the platform supports multiple deployments with different client_id values for the tool.
 		// However, this is not technically required by the specifications. Hmm.
-		
-		Deployment d = null;  // this will ultimately be the return value
-		
-		if (platform_id.endsWith("/")) platform_id = platform_id.substring(0,platform_id.length()-1);
-		
-		URL platform = new URL(platform_id);
-		if (!platform.getProtocol().equals("https")) throw new Exception("The platform_id must be a secure URL.");
-		
-		// Take the optimistic route first; this should always work if the deployment_id has been provided, else return null;
-		if (deployment_id != null) {
-			String platform_deployment_id = platform_id + "/" + deployment_id;
-			d = ofy().load().type(Deployment.class).id(platform_deployment_id).now();
-			if (d == null) {
-				try {  // look for a deployment with a blank deployment_id from this platform
-					d = ofy().load().type(Deployment.class).id(platform_id + "/").safe();
-					ofy().delete().entity(d);
-					d.platform_deployment_id = platform_id + "/" + deployment_id;
-					d.created = new Date();
-					ofy().save().entity(d).now();
-				} catch (Exception e) {
+		Deployment d = null;
+		try {
+			String platform_id = request.getParameter("iss");   // this should be the platform_id URL (aud)
+			if (platform_id.endsWith("/")) platform_id = platform_id.substring(0,platform_id.length()-1);  // strip any trailing / from platform_id
+			
+			URL platform = new URL(platform_id);
+			if (!platform.getProtocol().equals("https")) throw new Exception("The platform_id must be a secure URL.");
+			
+			// Take the optimistic route first; this should always work if the deployment_id has been provided, else return null;
+			String deployment_id = request.getParameter("lti_deployment_id");
+			if (deployment_id != null) {
+				String platform_deployment_id = platform_id + "/" + deployment_id;
+				d = ofy().load().type(Deployment.class).id(platform_deployment_id).now();
+				if (d == null) {
+					try {  // look for a deployment with a blank deployment_id from this platform
+						d = ofy().load().type(Deployment.class).id(platform_id + "/").safe();
+						ofy().delete().entity(d);
+						d.platform_deployment_id = platform_id + "/" + deployment_id;
+						d.created = new Date();
+						ofy().save().entity(d).now();
+					} catch (Exception e) {
+					}
 				}
+				return d;
 			}
-			return d;
-		}
 
-		// DeploymentId was not sent; prepare to search for all deployments from this platform:
-		Key<Deployment> kstart = Key.create(Deployment.class, platform_id);
-		Key<Deployment> kend = Key.create(Deployment.class, platform_id + "~");			
-		List<Deployment> deployments = null;
-		if (client_id != null) {
-			// Find all deployments from this platform with the specified client_id; there SHOULD be only one if the deployment_id was not provided.
-			deployments = ofy().load().type(Deployment.class).filterKey(">=",kstart).filterKey("<",kend).filter("client_id",client_id).list();
-			if (deployments.size()==1) return deployments.get(0);
-		} else {
-			// Find all of the deployments from this platform; there SHOULD be only one if neither deployment_id nor client_id was provided.
-			deployments = ofy().load().type(Deployment.class).filterKey(">=",kstart).filterKey("<",kend).list();
-			if (deployments.size()==1) return deployments.get(0);
-		}
-		
-		if (d==null) {  // Still no joy. Check to see if there might be an error in the platform_id or iss value (e.g., using an instructure test platform)
-			deployments =  ofy().load().type(Deployment.class).filter("client_id",client_id).list();
-			if (deployments.size()==1) {
-				d = deployments.get(0);
-				if ("canvas".equals(d.lms_type) && platform_id.contains("instructure.com") && d.lastLogin==null) {
-					// create new Deployment with the correct platform_id and access points
-					String oidc_auth_url = platform_id + "/api/lti/authorize_redirect";
-					String oauth_access_token_url = platform_id + "/login/oauth2/token";
-					String well_known_jwks_url = platform_id + "/api/lti/security/jwks";
-					String client_name = d.contact_name;
-					String email = d.email;
-					String organization = d.organization;
-					String org_url = d.org_url;
-					String lms = d.lms_type;
-					Deployment d2 = new Deployment(platform_id,d.getDeploymentId(),client_id,oidc_auth_url,oauth_access_token_url,well_known_jwks_url,client_name,email,organization,org_url,lms);				
-					ofy().save().entity(d2).now();
-					ofy().delete().entity(d);
-					return d2;
-				}
+			// DeploymentId was not sent; prepare to search for all deployments from this platform:
+			Key<Deployment> kstart = Key.create(Deployment.class, platform_id);
+			Key<Deployment> kend = Key.create(Deployment.class, platform_id + "~");			
+			String client_id = request.getParameter("client_id");
+			
+			List<Deployment> deployments = null;
+			if (client_id != null) {
+				// Find all deployments from this platform with the specified client_id; there SHOULD be only one if the deployment_id was not provided.
+				deployments = ofy().load().type(Deployment.class).filterKey(">=",kstart).filterKey("<",kend).filter("client_id",client_id).list();
+				if (deployments.size()==1) return deployments.get(0);
+			} else {
+				// Find all of the deployments from this platform; there SHOULD be only one if neither deployment_id nor client_id was provided.
+				deployments = ofy().load().type(Deployment.class).filterKey(">=",kstart).filterKey("<",kend).list();
+				if (deployments.size()==1) return deployments.get(0);  // this must be the registered deployment
 			}
+			
+			// Next, check the login_hint for a clue to the deployment. Schoology puts the deployment_id here
+			String login_hint = request.getParameter("login_hint");
+			try {
+				deployment_id = login_hint;
+				String platform_deployment_id = platform_id + "/" + deployment_id;
+				d = ofy().load().type(Deployment.class).id(platform_deployment_id).safe();
+				return d;
+			} catch (Exception e) {
+			}
+						
+			if (d==null) {  // Still no joy. Check to see if there might be an error in the platform_id or iss value (e.g., using an instructure test platform)
+				deployments =  ofy().load().type(Deployment.class).filter("client_id",client_id).list();
+				if (deployments.size()==1) {
+					d = deployments.get(0);
+					if ("canvas".equals(d.lms_type) && platform_id.contains("instructure.com") && d.lastLogin==null) {
+						// create new Deployment with the correct platform_id and access points
+						String oidc_auth_url = platform_id + "/api/lti/authorize_redirect";
+						String oauth_access_token_url = platform_id + "/login/oauth2/token";
+						String well_known_jwks_url = platform_id + "/api/lti/security/jwks";
+						String client_name = d.contact_name;
+						String email = d.email;
+						String organization = d.organization;
+						String org_url = d.org_url;
+						String lms = d.lms_type;
+						Deployment d2 = new Deployment(platform_id,d.getDeploymentId(),client_id,oidc_auth_url,oauth_access_token_url,well_known_jwks_url,client_name,email,organization,org_url,lms);				
+						ofy().save().entity(d2).now();
+						ofy().delete().entity(d);
+						return d2;
+					}
+				}
+			}		
+		} catch (Exception e) {
+			
 		}
 		
 		return d;
 	}
-	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
