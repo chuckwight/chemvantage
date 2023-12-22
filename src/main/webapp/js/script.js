16394effe78d48ebaa849316439ea55de5ab82af
rifatbhuiya567 <rifatbhuiya.personal@gmail.com>
2023-12-22 17:53:32 +0600

    updating last changes

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,60 +1,75 @@
-const f = () => {
-  let bttBtn = document.querySelector('.btt img');
+let bttBtn = document.querySelector('.btt img');
   
-  window.onload = () => {
-    bttBtn.style.display = 'none';
-  };
-  // Window Load
-  
-  window.onscroll = () => {
-    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-      document.querySelector(".webpage-header").style.padding = "6px 0";
-      document.querySelector(".webpage-logo").style.width = "180px";
-    } else {
-      document.querySelector(".webpage-header").style.padding = "8px 0";
-      document.querySelector(".webpage-logo").style.width = "200px";
-    }
-  
-    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      bttBtn.style.display = 'block';
-    }else {
-      bttBtn.style.display = 'none';
-    }
-  };
-  // Window Scroll
+window.onload = () => {
+  bttBtn.style.display = 'none';
+};
+// Window Load
 
-  const btt = () => {
-    bttBtn.addEventListener('click', () => {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    });
+window.onscroll = () => {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.querySelector(".webpage-header").style.padding = "6px 0";
+    document.querySelector(".webpage-logo").style.width = "180px";
+  } else {
+    document.querySelector(".webpage-header").style.padding = "8px 0";
+    document.querySelector(".webpage-logo").style.width = "200px";
   }
-  btt();
-  // BackToTop
 
-  const responsiveNav = () => {
-    let bar = document.querySelector('.solid-bar'),
-      close = document.querySelector('.close'),
-      navList = document.querySelector('.nav-list'),
-      header = document.querySelector('.header-nav');
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    bttBtn.style.display = 'block';
+  }else {
+    bttBtn.style.display = 'none';
+  }
+};
+// Window Scroll
 
-    bar.addEventListener("click", () => {
-      if (navList.style.display === 'block') {
-        navList.style.display = 'none';
-        header.classList.remove("collapsed");
-      }else {
-        navList.style.display = 'block';
-        header.classList.add("collapsed");
-      }
-    });
+const btt = () => {
+  bttBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+}
+btt();
+// BackToTop
 
-    close.addEventListener('click', () => {
+const responsiveNav = () => {
+  let bar = document.querySelector('.solid-bar'),
+    close = document.querySelector('.close'),
+    navList = document.querySelector('.nav-list'),
+    header = document.querySelector('.header-nav');
+
+  bar.addEventListener("click", () => {
+    if (navList.style.display === 'block') {
       navList.style.display = 'none';
       header.classList.remove("collapsed");
-    });
-  }
-  responsiveNav();
-  // Responsive Menu
-};
-f();
+    }else {
+      navList.style.display = 'block';
+      header.classList.add("collapsed");
+    }
+  });
 
+  close.addEventListener('click', () => {
+    navList.style.display = 'none';
+    header.classList.remove("collapsed");
+  });
+}
+responsiveNav();
+// Responsive Menu
+
+const lmsOption = (evt, tabItems) => {
+  var i, tabContent, tabLinks;
+  tabContent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+  }
+
+  tabLinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+  
+  document.getElementById(tabItems).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Install Tabs
